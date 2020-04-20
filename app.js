ea1e356374a1ba56c1967adbd0fca6f78626ac69
Aleksandar <aleksandar8nikolovski@gmail.com>
2020-04-20 13:30:51 +0200

    start app.js working - litenenig server  port

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const new = 'aleksandr';
+var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
+
+ var app = express();
+
+ app.use(morgan('tiny'));
+
+ app.get('/', function(req, res){
+   res.send('This a WEB APP');
+ });
+
+ app.listen(7777, function(){
+   debug(`Server listening on port ${chalk.green('5000')}`);
+ });
